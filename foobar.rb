c18a0cd87e826ad0b5992eac96b360b6a37efb14
allan942 <31258185+allan942@users.noreply.github.com>
2017-09-15 20:31:46 -0700

    Add files via upload

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,13 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    int_a = a.collect{|s| s.to_i}
+    add_two_a = int_a.map{|e| e += 2}
+    odd_a = add_two_a.delete_if {|n| n % 2 == 1}
+    uniq_a = odd_a.uniq
+    small_a = uniq_a.delete_if {|n| n > 10}
+    return small_a.inject(0, :+)
+    
   end
 end
 
